11685db616bb8b38d2f177c2420f2c957c91e820
sahil <jangras777@gmail.com>
2022-09-21 11:24:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+
+import './App.css';
+
+import React, { useState } from 'react'
+
+import Navbar from "./components/Navbar";
+import TextForms from "./components/TextForms";
+import Alert from './components/Alert';
+
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1800);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      showAlert("dark mode has been enabled", "success")
+    } else {
+      setMode('light');
+      showAlert("light mode has been enabled", "success")
+    }
+  }
+  return (
+    <>
+      <Router>
+        <Navbar title="my-App" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+        <Route path="" element={<TextForms heading="enter a text to analyze" />}/>
+          <Route path="/action" element={<About/>}/>
+            
+          
+
+          
+            
+          
+        </Routes>
+
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForms.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+
+export default function TextForms(props) {
+  const [text, setText] = useState("");
+
+  // text = "new text" = wrong way to change the state
+  // setText("new  text")  = right way  to change  the state
+  const handleOnChange = (event) => {
+    // console.log("onchange")
+    setText(event.target.value)
+
+
+  }
+
+  const handleupClick = () => {
+    // console.log("handleupClick")
+    let newtext = text.toUpperCase();
+    setText(newtext)
+    
+  }
+  const handleLoClick = () => {
+    // console.log("handleupClick")
+    let newtext = text.toLowerCase();
+    setText(newtext)
+  }
+  const handleclear = () => {
+    // console.log("handleupClick")
+    let newtext = '';
+    setText(newtext)
+  }
+  const handleCopy = () => {
+    // console.log("handleupClick")
+    let copytext = text
+    navigator.clipboard.writeText(text)
+    alert("copied " + copytext)
+  }
+  return (
+    <>
+      <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8" placeholder='enter text here'></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleupClick}>convrt to upper case</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>convrt to lower case</button>
+        <button className="btn btn-primary mx-2" onClick={handleclear}>clear text</button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>copy text</button>
+
+      </div>
+      <div className="container">
+        <h1>your text summary</h1>
+
+        <p>{text.split(" ").length} words  and  {text.length} characters </p>
+      </div>
+    </>
+  );
+}  
